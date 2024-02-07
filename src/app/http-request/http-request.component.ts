1d760bdfd7a62d88f3ba54a19dbfa733d17dca6a
K <251600@NTTDATA.COM>
2024-02-07 18:44:32 +0530

    The api request of get as well as post method was done

--- src/app/http-request/http-request.component.ts
+++ src/app/http-request/http-request.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-http-request',
@@ -11,9 +12,13 @@ import { HttpClient } from '@angular/common/http';
 //  refresh itself we need to avoid this issue we use the HTTPClient module from angular core package..
 export class HttpRequestComponent implements OnInit {
 
-  users:User[] = []
+  users: User[] = []
+  postForm = new FormGroup({
+    firstName: new FormControl(),
+    email: new FormControl()
+  })
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
 
   }
 
@@ -24,11 +29,25 @@ export class HttpRequestComponent implements OnInit {
     })
   }
 
-  getuserDetails(){
+  getuserDetails() {
     return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+  }
+
+  onSubmit() {
+    this.postRequest().subscribe((response) => {
+      this.users.push(response)
+    })
+  }
+
+  postRequest() {
+    return this.http.post<User>('https://jsonplaceholder.typicode.com/users', {
+      name: this.postForm.controls.firstName.value,
+      email: this.postForm.controls.email.value
+    })
   }
 }
 
 class User {
-  name!: String
+  name!: String;
+  email!: string;
 }
