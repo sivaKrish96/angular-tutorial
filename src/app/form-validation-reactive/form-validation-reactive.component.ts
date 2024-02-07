21f06d5af914a4591d3bec9bcf013c179d62db49
K <251600@NTTDATA.COM>
2024-02-07 17:24:13 +0530

    The error handling and form validation on reactive forms as well as in template forms was done

--- src/app/form-validation-reactive/form-validation-reactive.component.ts
+++ src/app/form-validation-reactive/form-validation-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-validation-reactive',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-validation-reactive.component.css']
 })
 export class FormValidationReactiveComponent {
+  contactForm = new FormGroup({
+    // here we are adding the validators for every field you may have to add the more than one varibale also 
+    // you able to add it as a second argument to the FormControl 
+    firstName: new FormControl('',[Validators.required, Validators.minLength(10)]),
+    lastName: new FormControl('', [Validators.required]),
+    address: new FormGroup({
+      city: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required])
+    })
+  })
 
+  get firstname(){
+    return this.contactForm.get('firstName')
+  }
+
+  onSubmit(){
+    console.log(this.contactForm)
+  }
 }
